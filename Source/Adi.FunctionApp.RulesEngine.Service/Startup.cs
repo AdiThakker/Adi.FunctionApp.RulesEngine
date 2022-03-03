6980d1ee990cc7b56958269c63d74af8ba82d5e9
Adi Thakker <Aditya.Thakker@gmail.com>
2022-03-03 11:01:01 -0500

    Add project files.

---
+++ Source/Adi.FunctionApp.RulesEngine.Service/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+
+[assembly:FunctionsStartup(typeof(Adi.FunctionApp.RulesEngine.Service.Startup))]
+namespace Adi.FunctionApp.RulesEngine.Service
+{
+
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // TODO Register Dependencies
+            throw new System.NotImplementedException();
+        }
+    }
+}
