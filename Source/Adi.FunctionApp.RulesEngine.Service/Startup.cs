8e3aa8f0f8cad2c49f3eb7a2b7240224767148fd
Adi Thakker <Aditya.Thakker@gmail.com>
2022-03-04 09:20:37 -0500

    added DI and execution logic

--- Source/Adi.FunctionApp.RulesEngine.Service/Startup.cs
+++ Source/Adi.FunctionApp.RulesEngine.Service/Startup.cs
@@ -1,4 +1,14 @@
-﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Adi.FunctionApp.RulesEngine.Domain.Builder;
+using Adi.FunctionApp.RulesEngine.Domain.Interfaces;
+using Adi.FunctionApp.RulesEngine.Domain.Models;
+using Adi.FunctionApp.RulesEngine.Domain.Rules;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 
 [assembly:FunctionsStartup(typeof(Adi.FunctionApp.RulesEngine.Service.Startup))]
 namespace Adi.FunctionApp.RulesEngine.Service
@@ -6,8 +16,47 @@ namespace Adi.FunctionApp.RulesEngine.Service
 
     public class Startup : FunctionsStartup
     {
+
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            FunctionsHostBuilderContext context = builder.GetContext();
+            builder.ConfigurationBuilder
+                    .AddJsonFile(Path.Combine(context.ApplicationRootPath, "appsettings.json"), optional: true, reloadOnChange: true)
+                    .AddJsonFile(Path.Combine(context.ApplicationRootPath, $"appsettings.{context.EnvironmentName}.json"), optional: true, reloadOnChange: true)
+                    .AddEnvironmentVariables();
+        }
+
         public override void Configure(IFunctionsHostBuilder builder)
         {
+            if (builder is null)
+                throw new ArgumentNullException(nameof(builder));
+
+            // Register dependencies
+            builder.Services.AddLogging();
+
+            // Register Rules
+            builder.Services.AddTransient(typeof(ForwardRule));
+            builder.Services.AddTransient(typeof(EscalateRule));
+
+            // Register Builder
+            builder.Services.AddOptions<RulesConfiguration>().Configure<IConfiguration>((settings, configuration) => configuration.GetSection(nameof(RulesConfiguration)).Bind(settings));  
+            builder.Services.AddSingleton<IRulesBuilder<RuleContext, RuleResult>>(sp =>
+            {
+                var configuration = sp.GetRequiredService<IOptions<RulesConfiguration>>();
+                Dictionary<string, IRule<RuleContext, RuleResult>> ruleLookup = new Dictionary<string, IRule<RuleContext, RuleResult>>
+                {
+                    { "Forward", sp.GetRequiredService<ForwardRule>() },
+                    { "Escalate", sp.GetRequiredService<EscalateRule>() }
+                };
+                return new RulesBuilder(configuration, ruleLookup);
+            });
+
+            // Register Executor
+
+
             // TODO Register Dependencies
             throw new System.NotImplementedException();
         }
---
+++ Source/Shared/Adi.FunctionApp.RulesEngine.Domain/Executor/RulesExecutor.cs
@@ -0,0 +1,51 @@
+﻿
+
+using System.Collections.Concurrent;
+using Adi.FunctionApp.RulesEngine.Domain.Interfaces;
+using Adi.FunctionApp.RulesEngine.Domain.Models;
+using Microsoft.Extensions.Logging;
+
+namespace Adi.FunctionApp.RulesEngine.Domain.Executor;
+
+public class RulesExecutor : IRulesExecutor<RuleContext, RuleResult>
+{
+
+    private ConcurrentDictionary<Func<RuleContext, bool>, (bool, IEnumerable<IRule<RuleContext, RuleResult>>)> rulesConfiguration;
+
+    private readonly ILogger<RulesExecutor> logger;
+
+    public RulesExecutor(IRulesBuilder<RuleContext, RuleResult> rulesBuilder, ILogger<RulesExecutor> loger)
+    {
+        if (rulesBuilder is null)
+            throw new ArgumentNullException(nameof(rulesBuilder));
+
+        logger = loger ?? throw new ArgumentNullException(nameof(loger));
+
+        if (rulesConfiguration is null)
+            rulesConfiguration = (ConcurrentDictionary<Func<RuleContext, bool>, (bool, IEnumerable<IRule<RuleContext, RuleResult>>)>)rulesBuilder.Build();    
+        
+    }
+
+    public IEnumerable<Task<RuleResult>> ExecuteAsync(RuleContext input)
+    {
+        IEnumerable<IRule<RuleContext,RuleResult>>? GetRulesToExecute(RuleContext input)
+        {
+            // Get Rules to execute
+            var matches = rulesConfiguration
+                            .Where(configuration => configuration.Key(input))
+                            .Select(criteria => new { Priority = criteria.Value.Item1, Rules = criteria.Value.Item2 });
+
+            if (matches.Any())
+                return matches.FirstOrDefault(match => match.Priority == true)?.Rules ?? matches.FirstOrDefault()?.Rules;
+
+            return default;
+        };
+
+        var rules = GetRulesToExecute(input);
+        if(rules != null && rules.Any())
+            return rules.Select(rule => rule.ExecuteAsync(input));
+
+        throw new InvalidOperationException($"No rule to execute for {input.Source} with {input.ContextType}");
+    }
+}
+
